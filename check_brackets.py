cab1c650f13c41466cffe375f36160606773bf2a
Michael S Lee <110694553+michaelsblee@users.noreply.github.com>
2023-09-28 17:43:35 -0400

    finished

--- check_brackets.py
+++ check_brackets.py
@@ -14,12 +14,25 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
-
+            b_l = Bracket(char=next, position=i)
+            opening_brackets_stack.append(b_l)
         if next in ")]}":
             # Process closing bracket, write your code here
+            if len(opening_brackets_stack) == 0:
+                print((i + 1))
+                return i + 1
+            top = opening_brackets_stack.pop()
+            if not are_matching(top[0], next):
+                print((i + 1))
+                return i + 1
+        else:
             pass
-
+    if len(opening_brackets_stack) != 0:
+        x = opening_brackets_stack[0]
+        print(x[1] + 1)
+        return x[1] + 1
+    print("Success")
+    return "Success"
 
 def main():
     text = input()
